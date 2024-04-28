c9c9606d7af05278ea80a5fddc4cabadbc8ca4a3
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2024-04-28 19:29:23 +0100

    routing metadata

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,4 @@
+export default function PrivateRoute() {
+    return <h1>You cannot </h1>
+}
+
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,24 @@
+import { Metadata } from "next";
+
+type Props = {
+    params: {
+        productId: string;
+    }
+}
+ 
+export const generateMetadata = async ({
+    params,
+}: Props): Promise<Metadata> => {
+    const title = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`iPhone ${params.productId}`)
+        }, 100);
+    });
+    return {
+        title: `Product ${title}`,
+    };
+}
+
 export default function ProductDetails( { 
     params,
 }: {
@@ -5,3 +26,4 @@ export default function ProductDetails( {
 } ) {
     return <h1>Details about product { params.productId} </h1>
 }
+
